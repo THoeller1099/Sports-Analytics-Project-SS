5d2416d4d82894ff501a326a57e3e43dfa272c5f
hpratt <ubuntu@ip-172-31-44-164.us-east-2.compute.internal>
2019-10-17 00:48:38 +0000

    read players from csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# seeding for players table
+# original seeding for players table
+=begin
     player1 = {first_name: "Lebron", last_name: "James", team: "Lakers", points: 156.0, assists: 81.0, rebounds: 100.0}
     player2 = {first_name: "James", last_name: "Harden", team: "Rockets", points: 144.0, assists: 79.0, rebounds: 85.0}
     player3 = {first_name: "Giannis", last_name: "Antetokounmpo", team: "Bucks", points: 142.0, assists: 85.0, rebounds: 112.0}
@@ -23,7 +24,27 @@
     player14 = {first_name: "Kyrie", last_name: "Irving", team: "Nets", points: 86.0, assists: 72.0, rebounds: 54.0}
     player15 = {first_name: "Bradley", last_name: "Beal", team: "Wizards", points: 83.0, assists: 61.0, rebounds: 57.0}
     Player.create([player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15])
-    
-    
-df = Daru::DataFrame.from_csv('data/NBApoints.csv')
-puts(df["Player"])
+=end
+
+# seeding from csv file
+df = Daru::DataFrame.from_csv('app/data/NBApoints.csv')
+nms = df["Player"]
+tms = df["Tm"]
+pts = df["PS/G"]
+asts = df["AST"]
+rbds = df["TRB"]
+
+nms.zip(tms, pts, asts, rbds).each do |nm, tm, pt, ast, rbd|
+	player = {first_name: "", last_name: "", team: "", points:0.0, assists:0.0, rebounds:0.0}
+	full_name = nm.split()
+	first = full_name[0]
+	last = full_name[1]
+	player[:first_name] = first
+	player[:last_name] = last
+	player[:team] = tm
+	player[:points] = pt
+	player[:assists] = ast
+	player[:rebounds] = rbd
+	Player.create([player])
+end
+
