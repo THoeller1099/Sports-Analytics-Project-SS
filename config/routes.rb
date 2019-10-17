0849d8b63aa702db21b14847e8a27cd906beccca
Ubuntu <ubuntu@ip-172-31-6-144.us-east-2.compute.internal>
2019-10-17 15:30:31 +0000

    Added home page with Player/Team Stats visible

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :teams
   resources :players
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'players#index'
+  root 'home#index'
 end
