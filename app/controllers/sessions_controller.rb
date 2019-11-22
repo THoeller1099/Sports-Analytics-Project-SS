84979f3f2aa1e4c92ff0a0774c7debe14720f908
Ubuntu <ubuntu@ip-172-31-6-144.us-east-2.compute.internal>
2019-11-22 02:42:45 +0000

    Created a Log in page, also displays current user's name when logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,33 +1,22 @@
 class SessionsController < ApplicationController
-    
-    def new
+  include SessionsHelper
+
+
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
-    
-    def create
-        user = User.find_by(email: params[:session][:email].downcase)
-        if user
-            googleAuth
-        else
-            render 'new'
-        end
-    end
-    
-    def destroy
-    end
-    
-    def googleAuth
-        # Get access tokens from the google server
-        access_token = request.env["omniauth.auth"]
-        user = User.from_omniauth(access_token)
-        login_url(user)
-        # Access_token is used to authenticate request made from the rails application to the google server
-        # user.google_token = access_token.credentials.token
-        # Refresh_token to request new access_token
-        # Note: Refresh_token is only sent once during the first request
-        refresh_token = access_token.credentials.refresh_token
-        user.google_refresh_token = refresh_token if refresh_token.present?
-        user.save
-        redirect_to root_path
-    end
-    
+  end
+  
+  def destroy
+  end
 end
+  
