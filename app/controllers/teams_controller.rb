e85381fdee0ecf499114f46e3936cf445d99c2bc
hpratt1 <hpratt99@gmail.com>
2019-12-04 02:38:41 +0000

    added pagination for all models

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,7 +5,7 @@ class TeamsController < ApplicationController
   # GET /teams
   # GET /teams.json
   def index
-    @teams = Team.all
+    @teams = Team.paginate(:page => params[:page], :per_page => 30)
     @players = Player.all
   end
 
