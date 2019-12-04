db19eb04d7d32e3d68bae5c0d48eb9286f4d624c
hpratt1 <hpratt99@gmail.com>
2019-12-04 02:13:20 +0000

    added homepage pagination

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem 'orderly'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+# add will_paginate gem
+gem 'will_paginate', '> 3.0'
+
 group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3', '1.4.1'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
   
-    @players = Player.where(year:"2018-2019").order(params[:sort]).reverse
-    @teams = Team.all
+    @players = Player.where(year:"2018-2019").order(params[:sort], :asc).paginate(:page => params[:page], :per_page => 25).reverse_order
+    @teams = Team.where(year: "2018-2019").paginate(:page => params[:page], :per_page => 10)
+    
   end
 end
